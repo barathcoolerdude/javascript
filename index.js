af2789771c53360a5e57fe6561382078cc4490a4
coolerdude <barathelango3@gmail.com>
2024-10-15 21:51:49 +0530

    dice roll program

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const textBox= document.getElementById("textBox");
-const result = document.getElementById("result");
-const toFahrenheit = document.getElementById("toFahrenheit");
-const toCelcious = document.getElementById("toCelcious");
 
-let temp;
-function convert(){
 
-    if(toFahrenheit.checked){
-        temp=Number(textBox.value);
-        temp = (temp*9/5) + 32;
-        result.textContent = temp.toFixed(2)+"F"
+
+
+function rollDice(){
+    const numberOfDice = document.getElementById("numberOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let i =0 ; i < numberOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png" alt="Dice ${values}" width="200" height="200" >`)
     }
-    else if(toCelcious){
-        temp=Number(textBox.value);
-        temp = (temp-32) * 5/9;
-        result.textContent=temp.toFixed(2)+"C";
-    }
-    else{
-        result.textContent = "please select a unit";
-    }
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join(' ');
 }
-
-
 
 
 
