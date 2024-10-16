12b262887f1ce84807687ff5cc27059ba6841f4e
coolerdude <barathelango3@gmail.com>
2024-10-16 15:29:37 +0530

    clock

--- index.js
+++ index.js
@@ -2,25 +2,3 @@
 
 
 
-function rollDice(){
-    const numberOfDice = document.getElementById("numberOfDice").value;
-    const diceResult = document.getElementById("diceResult");
-    const diceImages = document.getElementById("diceImages");
-    const values = [];
-    const images = [];
-
-    for (let i =0 ; i < numberOfDice; i++){
-        const value = Math.floor(Math.random() * 6) + 1;
-        values.push(value);
-        images.push(`<img src="images/${value}.png" alt="Dice ${values}" width="200" height="200" >`)
-    }
-    diceResult.textContent = `dice: ${values.join(', ')}`;
-    diceImages.innerHTML = images.join(' ');
-}
-
-
-
-
-
-
-
