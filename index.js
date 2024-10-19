92a0502d818cd6248f55def3d63d6bd417c870ac
coolerdude <barathelango3@gmail.com>
2024-10-19 19:08:15 +0530

    complete stopWatch

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
-const display = document.getElementById("display");
-let timer = null;
-let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
+let startTime = 0;
+
 
 function start(){
-    console.log("hello");
     if(!isRunning){
         startTime = Date.now() - elapsedTime;
         timer = setInterval(update , 10);
@@ -14,33 +11,30 @@ function start(){
     }
 }
 
-function pause(){
-    if(isRunning){
-        isRunning = false;
-    }
+function stop(){
+    clearInterval(timer);
+    isRunning = false;
 }
 
 function reset(){
-    timer = 0;
+    display.textContent = `00:00:00:00`;
     isRunning = false;
-    display.textContent = "00:00:00:00";
+
 }
 
 function update(){
-
     const currentTime = Date.now();
     elapsedTime = currentTime - startTime;
-
-    let hours = Math.floor(elapsedTime / (1000 * 60 *60));
-    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    
+    let hours =Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes =Math.floor( elapsedTime / (1000 * 60) % 60);
     let seconds = Math.floor(elapsedTime / 1000 % 60);
-    let milisecond = Math.floor(elapsedTime % 1000 /10);
+    let milisecond = Math.floor(elapsedTime % 1000 / 10);
 
     hours = String(hours).padStart(2 , "0");
     minutes = String(minutes).padStart(2 , "0");
     seconds = String(seconds).padStart(2 , "0");
     milisecond = String(milisecond).padStart(2 , "0");
-
-    display.textContent=`${hours}:${minutes}:${seconds}:${milisecond}`
-
+    
+    display.textContent = `${hours}:${minutes}:${seconds}:${milisecond}`
 }
