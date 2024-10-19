51d270db5960ab1388c5502644876a95323934d6
coolerdude <barathelango3@gmail.com>
2024-10-19 13:40:44 +0530

    stopwatch

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
-//random password generator
 
-let randomstring = "";
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-function generatePassword(){
-
-    const UpperCase = document.getElementById("upperCase");
-    const includeUpperCase = UpperCase.checked;
-    const LowerCase = document.getElementById("lowerCase");
-    const includeLowerCase = LowerCase.checked;
-    const Numbers = document.getElementById("numbers");
-    const includeNumbers = Numbers.checked;
-    const Symbols = document.getElementById("symbols");
-    const includeSymbols = Symbols.checked;
-    const length = document.getElementById("length").value;
-
-    const lowercaseChars= "abcdefghijklmnopqrstuvwxyz";
-    const uppercaseChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const numberChars= "0123456789";
-    const symbolChars = "@#$%^&"
-
-    let allowedChar = "";
-    let password = "";
-
-    allowedChar += includeLowerCase ? lowercaseChars :"e";
-    console.log(includeLowerCase);
-    allowedChar += includeUpperCase ? uppercaseChars :"e";
-    console.log(includeUpperCase);
-    allowedChar += includeNumbers ? numberChars :"e";
-    console.log(includeNumbers);
-    allowedChar += includeSymbols ? symbolChars :"e";
-    console.log(includeSymbols);
-
-    //console.log(allowedChar);
-    
-    for(let i = 0; i < length; i++){
-        const randomIndex = Math.floor(Math.random() * allowedChar.length);
-        //console.log(randomIndex);
-        password += allowedChar[randomIndex];
+function start(){
+    console.log("hello");
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update , 10);
+        isRunning = true;
     }
-
-    console.log(password);
-
-    document.getElementById("result").textContent = password;
-
 }
 
-    
-    
+function pause(){
+    if(isRunning){
+        isRunning = false;
+    }
+}
 
+function reset(){
+    timer = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
 
+function update(){
+
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 *60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milisecond = Math.floor(elapsedTime % 1000 /10);
+
+    hours = String(hours).padStart(2 , "0");
+    minutes = String(minutes).padStart(2 , "0");
+    seconds = String(seconds).padStart(2 , "0");
+    milisecond = String(milisecond).padStart(2 , "0");
+
+    display.textContent=`${hours}:${minutes}:${seconds}:${milisecond}`
+
+}
